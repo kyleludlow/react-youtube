b5c19fe1ffb04ef9bc8b11572651fcef84b1a4f2
Kyle <kyleludlow@gmail.com>
2016-08-16 20:15:10 -0700

    adds searchbar component, and youtube search to app component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBko0sgPAbpA3nD7Bgh5SGOILp_go6jV9s';
+
+
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      starter: 'Welcome to my React-Boiler.'
+      videos: []
     };
+
+    YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+      this.setState({ videos });
+    });
   }
 
   render() {
 
     return (
       <div>
-        {this.state.starter}
+        <SearchBar />
       </div>
     );
   }
